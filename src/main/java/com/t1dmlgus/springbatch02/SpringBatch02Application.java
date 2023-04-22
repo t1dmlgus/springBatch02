402238da2b9b83ebe8f93e2804970c384c4e5dd3
t1dmlgus <dmlgusgngl@gmail.com>
2023-04-22 13:00:10 +0900

    Initial commit

---
+++ src/main/java/com/t1dmlgus/springbatch02/SpringBatch02Application.java
@@ -0,0 +1,13 @@
+package com.t1dmlgus.springbatch02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatch02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatch02Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/t1dmlgus/springbatch02/SpringBatch02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.t1dmlgus.springbatch02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatch02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
